8b34a23e88cf9bb0e8b4843f63c67db9e7f6029d
DESKTOP-LFI4HU4\Asus <vimuth@soiposervices.com>
2018-09-03 14:28:15 +0530

    issue #3 - upload unit tests

--- tests/AppBundle/Controller/BookControllerTest.php
+++ tests/AppBundle/Controller/BookControllerTest.php
@@ -13,6 +13,28 @@ class BookControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/book/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+    
+    public function testNewBook() {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/book/new');
+        
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+
+//        $form = $crawler->filter('button.btn-success')->form(array(
+//            'appbundle_book[name]' => 'Book',
+//            'appbundle_book[price]' => '10',
+//            'appbundle_book[description]' => 'fffff',
+//            'appbundle_book[code]' => 'code',
+//        ));
+//        $crawler = $client->submit($form);
+//
+//        $this->assertTrue($client->getResponse()->isRedirection());
+//        $crawler = $client->followRedirect();
+//        $this->assertGreaterThan(
+//                0, $crawler->filter('html:contains("Book")')->count()
+//        );
     }
 }
--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,30 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        
+        $this->assertGreaterThan(
+                0, $crawler->filter('a#btnEmpty')->count()
+        );
+        
+        $this->assertGreaterThan(
+                0, $crawler->filter('select#book_category_category')->count()
+        );
+        
+        $this->assertGreaterThan(
+                0, $crawler->filter('div#product-grid')->count()
+        );
+        
+        $this->assertGreaterThan(
+                0, $crawler->filter('html:contains("Shopping Cart")')->count()
+        );
+    }
+    
+    public function testCheckout()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/checkout');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());      
     }
 }
